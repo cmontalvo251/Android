013e75b2050d9b1fc36be2577c54d68b417840f6
Carlos Montalvo <cmontalvo@southalabama.edu>
2023-07-02 15:17:42 -0500

    Added a few more questions and also a question counter

--- RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
+++ RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
@@ -14,9 +14,9 @@ import java.util.Collections;
 public class MainActivity extends AppCompatActivity {
     Random rand = new Random();
     List<String> Questions = new ArrayList<String>();
-    float NumberOfQuestions = 0;
+    int NumberOfQuestions = 0;
     int index = 0;
-    boolean RUN = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -140,15 +140,60 @@ public class MainActivity extends AppCompatActivity {
         Questions.add("What's your favorite flavor?");
         Questions.add("What does spending time in nature look like?");
         Questions.add("Do you prefer home cooked meals or a night out to order food? Or maybe carry out? Picnic?");
+        Questions.add("If you see a butterfly what do you do?");
+        Questions.add("What do you wear when you sleep?");
+        Questions.add("What temperature do you sleep at?");
+        Questions.add("Do you touch yourself at night?");
+        Questions.add("Do you eat eggs?");
+        Questions.add("When did your last relationship end and what did it teach you?");
+        Questions.add("Have you ever been in a relationship where your partner had strong emotional needs? How did you handle it?");
+        Questions.add("Do you feel comfortable explaining your emotions with someone you're newly dating?");
+        Questions.add("What is your favorite season?");
+        Questions.add("Tits or ass?");
+        Questions.add("Do you ever write handwritten letters? If so to whom? If not who would you do it to?");
+        Questions.add("What's your favorite museum? Which one is on your bucket list?");
+        Questions.add("What's your favorite sport to play?");
+        Questions.add("What's your favorite road trip? Where's your next road trip?");
+        Questions.add("What's your most prized possession?");
+        Questions.add("What's next on your wish list?");
+        Questions.add("If you started a business what would you sell?");
+        Questions.add("Whats the next movie you want to see?");
+        Questions.add("What person from history would you like to meet?");
+        Questions.add("What's your favorite board game?");
+        Questions.add("What's your favorite city you've visited?");
+        Questions.add("What fictional place do you want to visit?");
+        Questions.add("What's your favorite junk food?");
+        Questions.add("What junk food has no effect on you?");
+        Questions.add("What would you do during a significant natural disaster (like a fire sail)?");
+        Questions.add("Are you still friends with your ex? If not do you at least still talk?");
+        Questions.add("What's your weapon of choice during a zombie apocalypse?");
+        Questions.add("How important is money to you?");
+        Questions.add("Savings or paying off credit card debt?");
+        Questions.add("What were you like in high school? If we met in high school would we be friends?");
+        Questions.add("What's something super popular that you're not into?");
+        Questions.add("What's your biggest pet peeve?");
+        Questions.add("What are you like when there's a ton of traffic?");
+        Questions.add("What is our biggest inconvenience because other people ruined it?");
+        Questions.add("Do you believe in horoscopes?");
+        Questions.add("Do you believe in ghosts?");
+        Questions.add("Do you believe in conspiracy theories?");
+        Questions.add("If you could ask anyone a question and get a legitimate answer what and who would you ask?");
+        Questions.add("What are your non-negotiables and deal breakers?");
+        Questions.add("Do you have any favorite podcasts?");
         NumberOfQuestions = Questions.size();
         Collections.shuffle(Questions);
         setContentView(R.layout.activity_main);
     }
 
+    /**Called Everytime a question changes**/
+    public void SetQuestionCounter(View view) {
+        TextView textViewNumbers = findViewById(R.id.textViewNUMBERS);
+        String messageNumbers = "Question " + String.valueOf(index) + " out of " + String.valueOf(NumberOfQuestions);
+        textViewNumbers.setText(messageNumbers);
+    }
+
     /** Called when the user taps the Send button */
     public void GenerateQuestions(View view) {
-        RUN = true;
-        ///X BOX
         TextView textViewX = findViewById(R.id.textViewX);
         String message = Questions.get(index);
         index+=1;
@@ -156,12 +201,17 @@ public class MainActivity extends AppCompatActivity {
             index = 0;
         }
         textViewX.setText(message);
+        SetQuestionCounter(view);
     }
+
+    /** Called When user hits the reset button **/
     public void Reset(View view) {
-        RUN = false;
         TextView textViewX = findViewById(R.id.textViewX);
         String message = "Resetting Questions!!!";
         textViewX.setText(message);
+        //Reset counter to 0
+        index = 0;
+        SetQuestionCounter(view);
         Collections.shuffle(Questions);
     }
 }
