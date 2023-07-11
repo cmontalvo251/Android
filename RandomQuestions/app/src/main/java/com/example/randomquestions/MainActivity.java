9874bb5a18a2d94ece26a5692bdfa0a86b658d87
Carlos Montalvo <cmontalvo@southalabama.edu>
2023-07-11 16:30:57 -0500

    Added a bunch more marriage questions

--- RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
+++ RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
@@ -223,6 +223,8 @@ public class MainActivity extends AppCompatActivity {
 			Questions.add("Am I a safe person for you to talk to? How to make it better?");
 			Questions.add("Tell me a mistake in a relationship that taught you a lesson?");
 			Questions.add("How do you handle conflict?");
+			Questions.add("Have you ever been abused in any way (emotional or physical)?");
+			Questions.add("Do you have an addition to anything? (Drugs, alcohol, gambling, TV, porn, etc)");
 			Questions.add("How many kids do you want? Do you even wants kids? Adoption?");
 			Questions.add("What's your favorite thing about me?");
 			Questions.add("When did your last relationship end and what did it teach you?");
@@ -258,6 +260,11 @@ public class MainActivity extends AppCompatActivity {
 			Questions.add("Tits or ass?");
 			Questions.add("Does size matter?");
 			Questions.add("What does sex mean to you?");
+			Questions.add("Do you have any insecurities surrounding sex?");
+			Questions.add("Do you have any fears surrounding sex?");
+			Questions.add("Are there certain sexual activities that are off the table?");
+			Questions.add("What does intimacy mean to you?");
+			Questions.add("What does 'making love' mean to you?");
 		}
 		Switch swM = (Switch) findViewById(R.id.switchM);
 		boolean isCheckedM = swM.isChecked();
@@ -273,6 +280,7 @@ public class MainActivity extends AppCompatActivity {
 			Questions.add("How do you feel about entertaining people from work?");
 			Questions.add("How will you handle long distance travel? (Loneliness, kids, etc)?");
 			Questions.add("How do you feel about working late?");
+			Questions.add("How will you handle your finances? (who pays for what? , Separate bank accounts?, Debt?, Toys?)");
 			Questions.add("How do you feel about your spouse working with someone of the opposite sex?");
 			Questions.add("Rank these in order of importance: Career, Friends, Children, Marriage, Spirituality, Chores, Parents, Leisure");
 			Questions.add("What is your opinion about spending time with single friends (same sex) after you are married?");
@@ -280,10 +288,31 @@ public class MainActivity extends AppCompatActivity {
 			Questions.add("What is appropriate for you to share about your marriage to friends and family? What needs to be kept within the marriage?");
 			Questions.add("How often do you foresee seeing your parents?");
 			Questions.add("What will be your holiday plans after marriage?");
+			Questions.add("What are your goals for your marriage?");
+			Questions.add("Will you get jealous if I give someone of the opposite sex attention or even look at?");
+			Questions.add("Do you have a hard time trusting your partner?");
+			Questions.add("Do you feel like there are areas or topics that you cannot discuss with your partner?");
+			Questions.add("Do you have a problem apologizing to your partner or admitting fault?");
+			Questions.add("Do you hold grudges?");
+			Questions.add("Is there any outside pressure to get married?");
+			Questions.add("Agree or disagree: I can't handle being single");
+			Questions.add("What does 'Being faithful/cheating' mean to you in a marriage?");
+			Questions.add("Are there things your partner does that you hope they will change? What are they? Note: You cannot force someone to change.");
+			Questions.add("Can you relax and be yourself around your partner?");
+			Questions.add("Do you act differently at parties when your partner is there or not?");
+			Questions.add("Do you depend on each other for emotional support?");
+			Questions.add("If you have an issue with your partner, how do you bring it up? Are you loving or brutally honest?");
+			Questions.add("How much time will you spend together or apart? Hobbies? Alone time? Other friends and family?");
+			Questions.add("When you are making a decision and cannot agree, what will you do?");
+			Questions.add("How will you show each other affection (hand holding, hugs, etc)? This is different than the 5 love languages");
 			Questions.add("How close do you want to live ideally to your parents?");
 			Questions.add("Under what circumstances will you accept financial assistance from your parents?");
 			Questions.add("Under what circumstances will you offer financial assistance to your parents?");
 			Questions.add("If we plan on having kids, what will be the roles of the grandparents?");
+			Questions.add("If you already have children, how will you parent shared children?");
+			Questions.add("Does your family and extended family like and welcome your partner into their lives?");
+			Questions.add("Do you feel welcome in your partners family?");
+			Questions.add("If you found out your partner cheated on you, would you immediately get divorced?");
 			Questions.add("Will spirituality be important in your marriage? If so, how will you grow spiritually together (church, faith, ministries, prayer, etc)?");
 		}
 		//////////////UPDATE NUMBER OF QUESTIONS//////////////
