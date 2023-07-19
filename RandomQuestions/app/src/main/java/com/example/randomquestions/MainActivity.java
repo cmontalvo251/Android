62f184271c837a57a2193153c84fe9e3adff6a65
Carlos Montalvo Y700 Laptop <cmontalvo@southalabama.edu>
2023-07-19 08:05:04 -0500

    Added a few questions and renamed x-rated to physical relationship for questions app

--- RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
+++ RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
@@ -240,25 +240,27 @@ public class MainActivity extends AppCompatActivity {
 		boolean isCheckedXR = swXR.isChecked();
 		if (isCheckedXR) {
 			// The toggle is enabled
-			// Add X-Rated
-			//X-RATED
+			// Add Physical Relationship
+			//PHYSICAL RELATIONSHIP
 			Questions.add("Do you like massages? Where?");
 			Questions.add("What does physical intimacy look like to you?");
 			Questions.add("Cuddles or kisses?");
 			Questions.add("What do I do that turns you on the most?");
-			Questions.add("When did you parents talk to you about sex?");
+			Questions.add("When did your parents talk to you about sex?");
 			Questions.add("Tell me about your previous sexual partners");
 			Questions.add("Have you ever had a bad hookup?");
 			Questions.add("What part of your body do you most like to be touched?");
 			Questions.add("What do you do in the shower?");
 			Questions.add("Have you ever had a threesome?");
-			Questions.add("Craziest place you've had sex?");
-			Questions.add("Favorite sex position?");
+			Questions.add("Where is the craziest place you've had sex?");
+			Questions.add("What is your favorite sex move?");
+			Questions.add("What is your favorite sex position?");
 			Questions.add("What's your favorite part of my body?");
-			Questions.add("Porn and/or masturbation?");
+			Questions.add("How do you feel about your partner watching porn without you in a committed relationship?");
+			Questions.add("How do you feel about your partner masturbating in a committed relationship?");
 			Questions.add("Do you touch yourself at night?");
-			Questions.add("Tits or ass?");
-			Questions.add("Does size matter?");
+			Questions.add("Do you prefer tits or ass?");
+			Questions.add("Does size matter (height, dick, boobs, ass, weight)?");
 			Questions.add("What does sex mean to you?");
 			Questions.add("Do you have any insecurities surrounding sex?");
 			Questions.add("Do you have any fears surrounding sex?");
