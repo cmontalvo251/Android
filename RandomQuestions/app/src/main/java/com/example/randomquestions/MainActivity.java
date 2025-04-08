c95ff4a26574a14996926da3653740ff0fec2622
Carlos Montalvo PRESTIGE <cmontalvo251@gmail.com>
2025-04-08 15:38:32 -0500

    Added questions game to MeerKat

---
+++ RandomQuestions/app/src/main/java/com/example/randomquestions/MainActivity.java
@@ -0,0 +1,114 @@
+package com.example.randomquestions;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+///Edits
+// 10/27/2024 - 310 questions but added 4 questions to the marriage category. There are now 314 questions
+// 03/08/2025 - Added 3 questions so we're at 317 questions now - Also upgraded to MeerKat
+public class MainActivity extends AppCompatActivity {
+    Random rand = new Random();
+    List<String> Questions = new ArrayList<String>();
+    int NumberOfQuestions = 0;
+    int index = 0;
+	//Create Question Classes
+	Politics_and_Religion PR = new Politics_and_Religion();
+	Family_Friendly FF = new Family_Friendly();
+	Casually_Dating CD = new Casually_Dating();
+	Seriously_Dating SD = new Seriously_Dating();
+	XRated XR = new XRated();
+	Marriage M = new Marriage();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**Called Everytime a question changes**/
+    public void SetQuestionCounter(View view) {
+        TextView textViewNumbers = findViewById(R.id.textViewNUMBERS);
+        String messageNumbers = "Question " + String.valueOf(index) + " out of " + String.valueOf(NumberOfQuestions);
+        textViewNumbers.setText(messageNumbers);
+    }
+
+    /** Called when the user taps the Send button */
+    public void GenerateQuestions(View view) {
+        TextView textViewX = findViewById(R.id.textViewX);
+		String message = "You need to hit the reset button first";
+		if (NumberOfQuestions > 0) {
+			message = Questions.get(index);
+			index += 1;
+			if (index > NumberOfQuestions - 1) {
+				index = 0;
+			}
+		}
+        textViewX.setText(message);
+        SetQuestionCounter(view);
+    }
+
+    /** Called When user hits the reset button **/
+    public void Reset(View view) {
+		//Reset number of questions
+		NumberOfQuestions = 0;
+		Questions.clear();
+		//Check status of switches
+		Switch swFF = (Switch) findViewById(R.id.switchFF);
+		boolean isCheckedFF = swFF.isChecked();
+		if (isCheckedFF) {
+			// The toggle is enabled
+			FF.AddQuestions(Questions);
+		}
+		Switch swPR = (Switch) findViewById(R.id.switchPR);
+		boolean isCheckedPR = swPR.isChecked();
+		if (isCheckedPR) {
+			// The toggle is enabled
+			PR.AddQuestions(Questions);
+		}
+		Switch swCD = (Switch) findViewById(R.id.switchCD);
+		boolean isCheckedCD = swCD.isChecked();
+		if (isCheckedCD) {
+			// The toggle is enabled
+			CD.AddQuestions(Questions);
+		}
+		Switch swSD = (Switch) findViewById(R.id.switchSD);
+		boolean isCheckedSD = swSD.isChecked();
+		if (isCheckedSD) {
+			// The toggle is enabled
+			SD.AddQuestions(Questions);
+		}
+		Switch swXR = (Switch) findViewById(R.id.switchXR);
+		boolean isCheckedXR = swXR.isChecked();
+		if (isCheckedXR) {
+			// The toggle is enabled
+			XR.AddQuestions(Questions);
+		}
+		Switch swM = (Switch) findViewById(R.id.switchM);
+		boolean isCheckedM = swM.isChecked();
+		if (isCheckedM) {
+			//Questions to ask before you get married
+			M.AddQuestions(Questions);
+		}
+		//////////////UPDATE NUMBER OF QUESTIONS//////////////
+		index = 0;
+		NumberOfQuestions = Questions.size();
+		SetQuestionCounter(view);
+		//////////////UPDATE TEXT///////////////////////////
+		TextView textViewX = findViewById(R.id.textViewX);
+		String message = "Default";
+		if (NumberOfQuestions == 0) {
+			message = "You need to switch one of the toggles below!!!";
+		} else {
+			message = "Resetting Questions!!!";
+			Collections.shuffle(Questions);
+		}
+        textViewX.setText(message);
+    }
+}
---
+++ RandomQuestions/app/src/main/java/com/example/randomquestions/Seriously_Dating.java
@@ -0,0 +1,62 @@
+package com.example.randomquestions;
+import java.util.List;
+
+public class Seriously_Dating {
+    public void AddQuestions(List<String> Questions){
+        // Add Seriously Dating
+        //DATING SERIOUSLY
+        Questions.add("What roles do love and affection play in your life?");
+        Questions.add("Alternate sharing something you consider a positive characteristic of your partner. Share a total of 5 things each");
+        Questions.add("If I cheat on you and feel terrible about it, do you want me to tell you or deal with it on my own?");
+        Questions.add("When did you last cry in front of another person? By yourself?");
+        Questions.add("Do you want to know if an ex is contacting me or just keep it to myself?");
+        Questions.add("Do you want our phones to remain private or are they free to be looked at whenever you want?");
+        Questions.add("How do you feel about me maintaining friendships with people where I've had prior romantic relationships with?");
+        Questions.add("How do you define the boundaries between privavy in our relationship and sharing things with close friends and family?");
+        Questions.add("Do you want me to share all moments of doubt or only share when it becomes a problem?");
+        Questions.add("Do you want me to tell you if someone has a crush on me or you?");
+        Questions.add("How do you feel about innocently flirting with someone of the opposite sex?");
+        Questions.add("Do you want me to tell you when a close friend or family member doesn't like you");
+        Questions.add("How do you define emotional cheating?");
+        Questions.add("Describe your childhood trauma");
+        Questions.add("What are your expectations for communication throughout the day (texting, calls, check-ins)?");
+        Questions.add("Have you ever cheated on a past partner?");
+        Questions.add("If I do end up cheating on you would you rather just break up or discuss the issue and try and resolve it together?");
+        Questions.add("Do you want to know if an ex is contacting me?");
+        Questions.add("Do you want me to tell you when someone has a crush on me?");
+        Questions.add("How do you feel about me innocently flirting with other people?");
+        Questions.add("Do you want me to tell you if one of my friends or family members doesn't like you?");
+        Questions.add("How do you define emotional cheating and if I did emotionally cheat would you want me to tell you about it?");
+        Questions.add("Do you want me to share any doubts I have about the relationship or only bring it up if it becomes a big problem?");
+        Questions.add("How private does our relationship need to be in terms of sharing details of the relationship with friends or family?");
+        Questions.add("Do you want our phones remain private or is it something that you want to have free access too?");
+        Questions.add("How do you feel about me maintaining friendships with people I've had a prior romantic relationship with?");
+        Questions.add("What do I do that irritates you the most?");
+        Questions.add("How do you feel about saying 'I love you'?");
+        Questions.add("Would you consider ever going to couple's counseling?");
+        Questions.add("How do you feel about relationships with the opposite sex?");
+        Questions.add("Can you describe one of your previous relationships?");
+        Questions.add("What went wrong in your previous relationship?");
+        Questions.add("What do you think you could have done better in your previous relationship?");
+        Questions.add("What makes you feel better when you're sad or stressed?");
+        Questions.add("What's one thing that could make our relationship better?");
+        Questions.add("Am I a safe person for you to talk to? How to make it better?");
+        Questions.add("Tell me a mistake in a relationship that taught you a lesson?");
+        Questions.add("How do you handle conflict?");
+        Questions.add("What is a problem you are currently dealing with right now?");
+        Questions.add("How can I support you during difficult times in your life?");
+        Questions.add("Have you ever been abused in any way (emotional or physical)?");
+        Questions.add("Do you have an addition to anything? (Drugs, alcohol, gambling, TV, porn, etc)");
+        Questions.add("How many kids do you want? Do you even wants kids? Adoption?");
+        Questions.add("What's your favorite thing about me?");
+        Questions.add("When did your last relationship end and what did it teach you?");
+        Questions.add("Have you ever been in a relationship where your partner had strong emotional needs? How did you handle it?");
+        Questions.add("Are you still friends with your ex? If not do you at least still talk?");
+        Questions.add("Are you open to marriage? Why or why not?");
+        Questions.add("Are you willing to co-habitate before getting married?");
+        Questions.add("If we lived together, who would do what chores around the house?");
+        Questions.add("If we had kids what would be everyone's responsibility? Like traditional gender roles or not?");
+        Questions.add("What are the expectation for time with each other versus time with friends and family?");
+        Questions.add("Have you thought about asking the marriage questions? Maybe you're not thinking about marriage but there are some good ones in there.");
+    }
+}
